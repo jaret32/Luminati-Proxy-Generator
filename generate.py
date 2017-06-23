58b980e62b0f6a354d70176f2584bf94b677a795
jaret32 <jaret.c.boyer@gmail.com>
2017-06-23 15:23:38 -0700

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,3 +1,7 @@
+super_country = "us"
+
+peer_country = "us"
+
 user_name = ""
 
 zone_name = ""
@@ -10,6 +14,6 @@ with open('proxies.txt', 'w') as file:
 
     for i in range(num_proxies):
         
-        file.write('servercountry-us.zproxy.luminati.io:22225:lum-customer-%s-zone-%s-country-us-session-rand%d:%s\n' % (user_name, zone_name, i+1, zone_password))
+        file.write('servercountry-%s.zproxy.luminati.io:22225:lum-customer-%s-zone-%s-country-%s-session-rand%d:%s\n' % (super_country, user_name, zone_name, peer_country, i+1, zone_password))
 
 print("Proxies have been generated and stored in the proxies.txt file")
